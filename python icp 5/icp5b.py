ee7abaa28ee56bae69770ce1e8b9a083cc087fc5
Prathyusha <nsriprathyusha@gmail.com>
2018-09-21 17:25:41 -0500

    commit

--- python icp 5/icp5b.py
+++ python icp 5/icp5b.py
@@ -15,6 +15,7 @@ def create_cluster(X, centroid_pts):
             cluster[value] = [x]
     return cluster
 
+
 def calculate_new_center(cluster):
     keys =sorted(cluster.keys())
     newmu = np.array([(np.mean(cluster[k],axis = 0))for k in keys])
@@ -25,9 +26,9 @@ def matched(new_centroids, old_centroids):
 
 def Apply_Kmeans(X, K, N):
     # selecting random centroids from dataset and by number of clusters.
+    print(X)
     old_centroids = np.random.randint(N, size = K)
-    old_centroid_pts = np.array([X[i]for i in old_centroids])
-
+    old_centroid_pts = np.array([X[i]for i in old_centroids ])
     print("old :",old_centroids)
     print(old_centroid_pts)
 
@@ -54,6 +55,7 @@ def Apply_Kmeans(X, K, N):
 
 def plot_cluster(mu,cluster, itr):
     color = 10 * ['r.','g.','k.','c.','b.','m.']
+    print(color)
     print('Iteration number : ',itr)
     for l in cluster.keys():
         for m in range(len(cluster[l])):
@@ -62,20 +64,31 @@ def plot_cluster(mu,cluster, itr):
     plt.show()
 
 def init_graph(N, p1, p2):
-    X = np.array([(random.uniform(p1,p2),random.uniform(p1,p2))for i in range(N)])
+    X = np.array([(random.randint(p1,p2),random.randint(p1,p2))for i in range(N)])
     return X
 
 def Simulate_Clusters():
-    print(".........Starting Cluster Simulation.........")
-    N = 6 #int(input('Enter the number of points.......'))
-    K = 2 #int(input('Enter the number of Clusters.......'))
-    print('Now Enter the bounds for choosing uniform value....\n')
-    p1 = 100 #int(input('Enter the lower bound for points.......'))
-    p2 = 200 #int(input('Enter the upper bound for points.......'))
-    X = init_graph(N, p1, p2)
-    plt.scatter(X[:, 0], X[:, 1])
+    print(".........Small, Medium and Large are the T-shirt sizes.........")
+    Number_of_points = 6
+
+    #considered 2 clusters
+    Number_of_clusters = 2#int(input('Enter the number of Clusters.......'))
+    print('Below are lower and upper bound details for the points in each cluster\n')
+    #points_lower_bound = 100
+    #points_upper_bound = 200
+    #X = init_graph(Number_of_points, points_lower_bound, points_upper_bound)
+
+    # Created an array wfor features height and weight
+    X = np.array(
+        [[1.0,1.0],[1.5,2.0],[3.0,4.0],[5.0,7.0],[3.5,5.0],[4.5,5.0] ])
+    # plot them on the graph
+
+    plt.scatter(X[:, 0], X[:, 1], label='True Position')
+    #X = np.array([2,3], [4,2], [1,6],[5,4])
+    #plt.scatter(X[:, 0], X[:, 1],)
     plt.show()
-    temp = Apply_Kmeans(X, K, N)
+    temp = Apply_Kmeans(X, Number_of_clusters, Number_of_points)
+
 
 if __name__ == '__main__':
     Simulate_Clusters()
